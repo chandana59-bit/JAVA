2b56ef399c475d2dcb4366ad15b99fb4e6433dcc
donth <donth@CHANDANA>
2025-09-20 20:00:17 +0530

    Commit

---
+++ java/src/com/tnsif/day1and2/Operators.java
@@ -0,0 +1,16 @@
+package com.tnsif.day1and2;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		 int a = 50, b = 30;
+	        System.out.println(" The value of a + b is " + (a + b));
+	        System.out.println("The value of a - b is " + (a - b));
+	        System.out.println("The value of a * b  is " + (a * b));
+	        System.out.println("The value of a / b is " + (a / b));
+	        System.out.println("The value of a % b is " + (a % b));
+	    }
+
+	}
+
+
---
+++ java/src/com/tnsif/day1and2/Primenumbers.java
@@ -0,0 +1,25 @@
+package com.tnsif.day1and2;
+
+public class Primenumbers {
+
+	public static void main(String[] args) {
+		System.out.println("The prime numbers from 1to100 are:");
+		for (int num = 2; num <= 100; num++) {
+            int count = 0;
+
+            for (int i = 1; i <= num; i++) {
+                if (num % i == 0) {
+                    count++;
+                }
+            }
+
+            if (count == 2) {
+                System.out.print(num + " ");
+            }
+        }
+
+	}
+
+	}
+
+

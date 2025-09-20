928d8ae146dedf205254d40575953c16fe02e2f2
donth <donth@CHANDANA>
2025-09-20 14:22:09 +0530

    IfCondition

---
+++ java/src/com/tnsif/basics/IfCondition.java
@@ -0,0 +1,14 @@
+package com.tnsif.basics;
+
+public class IfCondition {
+
+		public static void main(String[] args) {
+			int num=15;
+			if(num>11) {
+				System.out.println("Number is Greater than 11");
+			}
+
+		}
+
+	}
+

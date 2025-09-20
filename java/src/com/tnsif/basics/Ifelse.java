fc01774bc7ca7453ff66b4ae823211eacebd90cf
donth <donth@CHANDANA>
2025-09-20 14:31:25 +0530

    Ifelse

---
+++ java/src/com/tnsif/basics/Ifelse.java
@@ -0,0 +1,18 @@
+package com.tnsif.basics;
+
+public class Ifelse {
+
+	public static void main(String[] args) {
+			int age=20;
+			if(age>=18) {
+				System.out.println("Eligible for voting");
+			}
+			else {
+				System.out.println("Not Eligible for voting");
+			}
+
+		}
+
+	}
+
+

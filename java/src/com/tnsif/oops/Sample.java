2b4df83a07f4428c6225a4c785806b65fb38e8fa
donth <donth@CHANDANA>
2025-09-21 15:44:30 +0530

    Commit

--- java/src/com/tnsif/oops/Sample.java
+++ java/src/com/tnsif/oops/Sample.java
@@ -1,11 +1,12 @@
 package com.tnsif.oops;
 
- public class Sample {
+public class Sample {
+
 		int a=20;
 		boolean b=true;
 		public void show() {
 			System.out.println("This is a show method");
 		}
-		
-	}
-
+	
+	
+}

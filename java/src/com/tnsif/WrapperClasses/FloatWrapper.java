38c605feca5f708f442593636d35f6c7e282c67b
donth <donth@CHANDANA>
2025-09-21 20:40:44 +0530

    Wrapper

---
+++ java/src/com/tnsif/WrapperClasses/FloatWrapper.java
@@ -0,0 +1,12 @@
+package com.tnsif.WrapperClasses;
+
+public class FloatWrapper {
+	 public static void main(String[] args) {
+	        float f = 12.5f;
+	        Float floatObj = f;
+	        float fVal = floatObj;
+	        System.out.println("Float object: " + floatObj);
+	        System.out.println("Primitive float: " + fVal);
+	    }
+
+}
